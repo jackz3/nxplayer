7f04cdd78d784ec851b480b902243ccd1059b667
jack <jackz3@163.com>
2023-09-23 12:11:43 +0800

    fix no display name

--- components/TransitionWrapper.tsx
+++ components/TransitionWrapper.tsx
@@ -3,15 +3,15 @@ import { usePathname } from 'next/navigation'
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 
-export default function ({ children }: { children: React.ReactNode }) {
+export default function TransitionWrapper({ children }: { children: React.ReactNode }) {
   const path = usePathname()
   return (
     <TransitionGroup className={'flex flex-col grow'}>
       <CSSTransition key={path} timeout={{ exit: 0, enter: 500 }} unmountOnExit classNames={{
         // appear: "opacity-0",
         // appearActive: "transition ease-in-out duration-500 opacity-100",
-        enter: "transform-gpu translate-x-24 opacity-0",
-        enterActive: "translate-x-0 opacity-100 transition duration-300 ease-in-out",
+        enter: "transform translate-x-36",
+        enterActive: "transform translate-x-0 transition-transform duration-300 ease-in-out",
         // enterDone: 'opacity-100',
         exit: 'opacity-0',
       }}>
